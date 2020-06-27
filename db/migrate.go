6d85bf281e8bc1b25ae2178ee8064b0109fee177
Enyerson Camero <blade_liger@hotmail.com>
2020-06-27 10:05:40 -0500

    Update migrate.go

--- db/migrate.go
+++ db/migrate.go
@@ -10,6 +10,8 @@ func MigrateDB() {
 
 	db := GetConnection()
 	defer db.Close()
+	fmt.Println("Drop models....")
+	db.DropTable(&models.User{})
 	fmt.Println("Migrating models....")
 	// Automigrate se encarga de migrar la base de datos sí no se ha migrado, y lo hace a partir del modelo
 	db.AutoMigrate(&models.User{})
