5c643d6f9f3661d213f4f26feed61e97697fb1af
Enyerson Camero <blade_liger@hotmail.com>
2020-06-27 09:49:26 -0500

    Update controllers.go

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -37,7 +37,7 @@ func GetUser(ctx iris.Context) {
 		response.SendResponse(ctx, http.StatusOK, j)
 	} else {
 		// Si no existe se envia un error 404
-		log.Println(user)
+		log.Println(json.Marshal(user))
 		response.SendErr(ctx, http.StatusNotFound)
 	}
 
