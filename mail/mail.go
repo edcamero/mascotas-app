caebef2a89d74baeb33683e573cb867b78e1477b
enyerson <blade_liger13@hotmail.com>
2022-12-06 22:49:07 -0500

    servicio de envio de correos

--- mail/mail.go
+++ mail/mail.go
@@ -3,16 +3,17 @@ package util
 import (
 	"fmt"
 	"net/smtp"
+	"os"
 )
 
 const ServerMails = "smtp.gmail.com"
 
-func sendMail(customerMail string, password string, emailProvider string, mailRecipients []string, body string) {
+func SendMail(emailProvider string, mailRecipients []string, body string) {
 
-	auth := smtp.PlainAuth("", customerMail, password, ServerMails)
+	auth := smtp.PlainAuth("", os.Getenv("EMAIL_FROM"), os.Getenv("SMTP_PASS"), os.Getenv("SMTP_HOST"))
 
 	msg := []byte(body)
-	error := smtp.SendMail(ServerMails+":587", auth, customerMail, mailRecipients, msg)
+	error := smtp.SendMail(os.Getenv("SMTP_HOST")+":"+os.Getenv("SMTP_PORT"), auth, os.Getenv("EMAIL_FROM"), mailRecipients, msg)
 	if error != nil {
 		fmt.Println("Error al enviar correo", error)
 	}
