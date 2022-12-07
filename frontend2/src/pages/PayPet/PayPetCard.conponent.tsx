80a63ab4876e3c215e72527a9ed53ac28e3aa980
enyerson <blade_liger13@hotmail.com>
2022-12-06 22:49:44 -0500

    fotos de los metodos de pagos

--- frontend2/src/pages/PayPet/PayPetCard.conponent.tsx
+++ frontend2/src/pages/PayPet/PayPetCard.conponent.tsx
@@ -1,4 +1,4 @@
-import { Grid, Card, CardMedia} from '@mui/material'
+import { Grid, Card, CardMedia } from '@mui/material'
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
 
@@ -17,12 +17,7 @@ const PayPetCard: React.FC<IPayPetCardProps> = ({ text, image, link }) => {
   return (
     <Grid item xs={4} container direction="row" justifyContent="center" alignItems="center">
       <Card sx={{ width: 250, marginTop: '2rem' }} onClick={handlingClick}>
-        <CardMedia
-          component="img"
-          height="350"
-          image={`${process.env.REACT_APP_API_URL?.slice(0, -4)}${image}`}
-          alt="img-card-pay"
-        />
+        <CardMedia component="img" height="350" image={image} alt="img-card-pay" />
       </Card>
     </Grid>
   )
--- frontend2/src/pages/PayPet/resource/paymentmethods.ts
+++ frontend2/src/pages/PayPet/resource/paymentmethods.ts
@@ -1,8 +1,11 @@
 
+import imageNequi from '../../../Static/images/nequi.png'
+import imageBancolombia from '../../../Static/images/bancolombia.png'
+import imageMercadoPago from '../../../Static/images/mercadopago.png'
 export interface IPaymentmethods {
     text: string
     image: string
     link?: string
 }
 
-export const paymentmethods: IPaymentmethods[] = [{ text: 'Nequi', image: `public/uploads//nequi.png` }, { text: 'Bancolombia', image: `public/uploads//bancolombia.png` }, { text: 'Mercado Pago', image: `public/uploads//mercadopago.png`, link:'donation' }]
+export const paymentmethods: IPaymentmethods[] = [{ text: 'Nequi', image: imageNequi }, { text: 'Bancolombia', image: imageBancolombia }, { text: 'Mercado Pago', image: imageMercadoPago, link:'donation' }]
